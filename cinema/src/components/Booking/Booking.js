af53e19e7f4597b6178ac2184577e200fd90cb05
ewnetutesfaye <eunetu2023@gmail.com>
2023-08-21 22:30:00 +0200

    good now only with some logic errors

--- cinema/src/components/Booking/Booking.js
+++ cinema/src/components/Booking/Booking.js
@@ -1,47 +1,51 @@
 import React, { useState } from 'react';
 
-const BookingForm = ({ users, addPerson, occupiedSeatsData }) => {
+const BookingForm = ({ users, addPerson, removePerson, occupiedSeatsData }) => {
   const [form, setForm] = useState({ firstName: '', lastName: '', age: '', seat: '' });
 
-  const handleChange = (field, value) => {
-    setForm({ ...form, [field]: value });
+  const availableSeats = Array.from({ length: occupiedSeatsData?.total || 0 })
+    .map((_, i) => i)
+    .filter(i => !occupiedSeatsData?.occupiedSeats.split(", ").includes(i.toString()));
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value
+    });
   }
 
-  const handleSubmit = () => {
+  const handleBook = () => {
     addPerson(form);
     setForm({ firstName: '', lastName: '', age: '', seat: '' });
   }
 
-  const getAvailableSeats = () => {
-    const occupiedSeats = occupiedSeatsData?.occupiedSeats.split(", ").map(Number);
-    return Array.from({ length: occupiedSeatsData?.total || 0 })
-      .map((_, i) => i)
-      .filter(seat => !occupiedSeats.includes(seat));
-  }
-
   return (
     <div className="bookingForm">
-      <div className="newUserForm">
-        <input placeholder="First Name" value={form.firstName} onChange={e => handleChange('firstName', e.target.value)} />
-        <input placeholder="Last Name" value={form.lastName} onChange={e => handleChange('lastName', e.target.value)} />
-        <input placeholder="Age" type="number" value={form.age} onChange={e => handleChange('age', e.target.value)} />
-        <select value={form.seat} onChange={e => handleChange('seat', e.target.value)}>
-          {getAvailableSeats().map(seat => <option key={seat} value={seat}>{seat + 1}</option>)}
+      <form>
+        <input name="firstName" placeholder="First Name" value={form.firstName} onChange={handleChange} />
+        <input name="lastName" placeholder="Last Name" value={form.lastName} onChange={handleChange} />
+        <input name="age" type="number" placeholder="Age" value={form.age} onChange={handleChange} />
+        <select name="seat" value={form.seat} onChange={handleChange}>
+          <option value="" disabled>Select Seat</option>
+          {availableSeats.map(seat => (
+            <option key={seat} value={seat}>{seat + 1}</option>
+          ))}
         </select>
-        <button onClick={handleSubmit}>Book</button>
-      </div>
-
-      <div className="bookedUsers">
-        {users.map((user, index) => (
-          <div key={index} className="userDetails">
-            {user.firstName} {user.lastName} - Seat {user.seat + 1}
+        <button type="button" onClick={handleBook}>Book</button>
+      </form>
+      <div>
+        <h2>Booked Customers</h2>
+        {users.map((user, i) => (
+          <div key={i}>
+            {user.firstName}, {user.lastName}, Seat: {user.seat + 1}, Price: SEK {user.age > 65 ? 75 : user.age < 12 ? 65 : 85}
+            <button onClick={() => removePerson(i)}>Remove</button>
           </div>
         ))}
       </div>
-
-      <button onClick={() => setForm({ firstName: '', lastName: '', age: '', seat: '' })}>Add Another Person</button>
+      
     </div>
   );
-}
+};
 
 export default BookingForm;
--- cinema/src/views/BookingPage/BookingPage.js
+++ cinema/src/views/BookingPage/BookingPage.js
@@ -6,7 +6,7 @@ import './BookingPage.css';
 
 const BookingPage = () => {
   const { screeningId } = useParams();
-  const [occupiedSeatsData, setOccupiedSeats] = useState(null);
+  const [occupiedSeatsData, setOccupiedSeatsData] = useState(null);
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
@@ -14,9 +14,8 @@ const BookingPage = () => {
       try {
         const allSeatsData = await fetchOccupiedSeats();
         const specificScreeningSeats = allSeatsData.find(seat => seat.screeningId.toString() === screeningId);
-
         if (specificScreeningSeats) {
-          setOccupiedSeats(specificScreeningSeats);
+          setOccupiedSeatsData(specificScreeningSeats);
         } else {
           console.warn(`No occupied seats found for screening ID: ${screeningId}`);
         }
@@ -36,25 +35,37 @@ const BookingPage = () => {
   }
 
   const addPerson = (person) => {
+    setOccupiedSeatsData({
+      ...occupiedSeatsData,
+      occupiedSeats: occupiedSeatsData.occupiedSeats + ", " + person.seat
+    });
     setUsers([...users, person]);
+  }
+
+  const removePerson = (index) => {
+    const seatToRemove = users[index].seat;
+    const updatedOccupiedSeats = occupiedSeatsData.occupiedSeats.split(", ").filter(seat => seat !== seatToRemove.toString()).join(", ");
+    setOccupiedSeatsData({ ...occupiedSeatsData, occupiedSeats: updatedOccupiedSeats });
+    setUsers(users.filter((_, i) => i !== index));
   }
 
   return (
     <div className="bookingPage">
-      <h2>{occupiedSeatsData?.movie}</h2>
-      <h3>{occupiedSeatsData?.screeningTime}</h3>
-      <h3>{occupiedSeatsData?.auditorium}</h3>
+      <h1>{occupiedSeatsData?.movie}</h1>
+      <h3>Date: {occupiedSeatsData?.screeningTime.slice(0, 10)}, Time: {occupiedSeatsData?.screeningTime.slice(11, 16)}</h3>
+      <h2>{occupiedSeatsData?.auditorium}</h2>
       <div className="seatsContainer">
         {Array.from({ length: occupiedSeatsData?.total || 0 }).map((_, i) => (
           <div 
             key={i}
             className={`seat ${occupiedSeatsData?.occupiedSeats.split(", ").includes(i.toString()) ? 'occupied' : 'available'}`}
+            style={{order: i % 15 }}
           >
             {i + 1}
           </div>
         ))}
       </div>
-      <BookingForm users={users} addPerson={addPerson} occupiedSeatsData={occupiedSeatsData} />
+      <BookingForm users={users} addPerson={addPerson} removePerson={removePerson} occupiedSeatsData={occupiedSeatsData} />
       <div className="totalPrice">
         Total Price: SEK {calculatePrice()}
       </div>
