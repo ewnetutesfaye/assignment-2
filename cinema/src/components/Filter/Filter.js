e4f80bc799347071cbb33b34726a21a4c83d5cf4
ewnetutesfaye <eunetu2023@gmail.com>
2023-08-21 03:45:07 +0200

    Home.js partially done

--- cinema/src/components/Filter/Filter.js
+++ cinema/src/components/Filter/Filter.js
@@ -16,4 +16,5 @@ const Filter = ({ categories, setFilter }) => {
     );
 };
 
+
 export default Filter;
--- cinema/src/views/Home/Home.js
+++ cinema/src/views/Home/Home.js
@@ -1,52 +1,53 @@
 import React, { useEffect, useState } from 'react';
 import ScreeningsList from '../../components/ScreeningsList/ScreeningsList';
 import Filter from '../../components/Filter/Filter';
-import { fetchScreenings, fetchMovies, fetchAuditoriums } from '../../tools/dataFetcher';
+import { fetchScreenings, fetchMovies, fetchAuditoriums, fetchCategories } from '../../tools/dataFetcher';
 import './Home.css';
 
 const Home = () => {
     const [screenings, setScreenings] = useState([]);
     const [movies, setMovies] = useState({});
     const [auditoriums, setAuditoriums] = useState({});
-    const [filter, setFilter] = useState('all');
+    const [categories, setCategories] = useState([]);
+    const [selectedFilter, setSelectedFilter] = useState('all');
+    const [error, setError] = useState(null);
+
+    const filteredScreenings = screenings.filter(screening => {
+        if (selectedFilter === 'all') return true;
+        const movie = movies[screening.movieId];
+        return movie?.description?.categories?.includes(selectedFilter);
+    });
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const screeningsData = await fetchScreenings();
                 setScreenings(screeningsData);
-
+    
                 const moviesData = await fetchMovies();
-                const moviesObj = {};
-                moviesData.forEach(movie => {
-                    moviesObj[movie.id] = movie;
-                });
-                setMovies(moviesObj);
+                setMovies(Object.fromEntries(moviesData.map(movie => [movie.id, movie])));
 
                 const auditoriumsData = await fetchAuditoriums();
-                const auditoriumsObj = {};
-                auditoriumsData.forEach(auditorium => {
-                    auditoriumsObj[auditorium.id] = auditorium;
-                });
-                setAuditoriums(auditoriumsObj);
-            } catch (error) {
-                console.error("Error fetching data: ", error);
+                setAuditoriums(Object.fromEntries(auditoriumsData.map(auditorium => [auditorium.id, auditorium])));
+    
+                const categoriesData = await fetchCategories();
+                setCategories(categoriesData);
+            } catch (err) {
+                setError('Error fetching data. Please try again later.');
+                console.error("Error fetching data: ", err);
             }
         };
-
         fetchData();
     }, []);
 
-    useEffect(() => {
-        // Handle logic for filtering screenings based on category
-        // ... 
-
-    }, [filter]);
+    if (error) {
+        return <div className="errorDiv">{error}</div>;
+    }
 
     return (
         <div className='homeDiv'>
-            
-            <ScreeningsList screenings={screenings} movies={movies} auditoriums={auditoriums} />
+            <Filter setFilter={setSelectedFilter} categories={categories} />
+            <ScreeningsList screenings={filteredScreenings} movies={movies} auditoriums={auditoriums} />
         </div>
     );
 }
