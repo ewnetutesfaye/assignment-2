104bb23c16a74f9e499e3d7cdf5cf386fc6dfc25
ewnetutesfaye <eunetu2023@gmail.com>
2023-08-23 04:04:58 +0200

    completed now

--- cinema/src/components/ScreeningsList/ScreeningsList.js
+++ cinema/src/components/ScreeningsList/ScreeningsList.js
@@ -5,14 +5,16 @@ import { Card, Button, Row, Col } from 'react-bootstrap';
 
 const ScreeningsList = ({ screenings, movies, auditoriums }) => {
 
+    const address_base = "https://cinema-rest.nodehill.se"
+
     return (
         <div className='ScreeningsList-box'>
             <Row className="justify-content-md-center">
                 {screenings.map(screening => (
                     <Col key={screening.id} sm={12} md={6} lg={4} className="mb-4">
                         <Card className='ScreeningsList-item'>
-                            <Card.Img variant="top" src={movies[screening.movieId]?.description.posterImage} alt={movies[screening.movieId]?.title} />
-                            <Card.Body>
+                        <Card.Img variant="top" src={address_base + movies[screening.movieId]?.description.posterImage} alt={movies[screening.movieId]?.title} />
+<Card.Body>
                                 <Card.Title>{movies[screening.movieId]?.title}</Card.Title>
                                 <Card.Subtitle className="mb-2 text-muted">{new Date(screening.time).toLocaleString()}</Card.Subtitle>
                                 <Card.Text>
@@ -20,7 +22,7 @@ const ScreeningsList = ({ screenings, movies, auditoriums }) => {
                                     Auditorium: {auditoriums[screening.auditoriumId]?.name}
                                 </Card.Text>
                                 <Link to={`/booking/${screening.id}`}>
-                                    <Button variant="primary">BOOK</Button>
+                                    <Button className='book' variant="primary">BOOK</Button>
                                 </Link>
                             </Card.Body>
                         </Card>
